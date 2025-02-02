385e1ebf8144da30429b0936c9f6d9435fcdd410
Tafintsev Feodor <taf.f11@ya.ru>
2025-02-02 22:28:57 +0300

    тесты скачивания таблицы

--- script.js
+++ script.js
@@ -1,76 +1,133 @@
 // ==UserScript==
-// @name         My School Color Point
+// @name         Test My School Color Point
 // @namespace    http://tampermonkey.net/
-// @version      2024-11-20
+// @version      2025-02-02
 // @description  Окрашивает оценки в разные цвета в Моя Школа
 // @author       Tafintsev Feodor taf.f11@ya.ru
 // @match        https://authedu.mosreg.ru/teacher/study-process/journal/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mosreg.ru
 // @grant        none
 // ==/UserScript==
-let mutationObserver = new MutationObserver(mutationCallback);
 
-function mutationCallback(mutations) {
+let OUT_STR_COLORIZE_TYPE; // строка для отображения типа выбранного округления
+let OUT_STR_TRIMESTR_INFO; // строка с информайией о количестве уроков в триместре и нужном количестве оценок для атестации
+let WATCH_ELEMENT;
+let WRONG_JOURNAL_LIST = [// список журналов в которых округление математическое
+    'Изобразительное искусство',
+    'Музыка',
+    'Труд',
+    'Физическая культура'
+];
+let MUTATION_OBSERVER = new MutationObserver(mutationCallback);
+
+function mutationCallback() {
     console.log("Возможно, таблица пропала. Запускаем поиск");
-    mutationObserver.disconnect();
+    MUTATION_OBSERVER.disconnect();
     setTimeout(getHim, 2000);
 };
 
-function getHim() {
+function isMathRoundType(nameJornal) { //определяет нужно ли применять математическое округление или использовать Лицейское
+    // проверяем есть ли в названии текущего журнала слова из списка журналов в которых применяется математическое округление
+    for (let i = 0; i < WRONG_JOURNAL_LIST.length; i++) {
+        if (nameJornal.includes(WRONG_JOURNAL_LIST[i])) {
+            OUT_STR_COLORIZE_TYPE = 'Выбран метод оценивания по правилам математики (0.5)'
+            console.log(OUT_STR_COLORIZE_TYPE);
+            return true
+        }
+    }
+    OUT_STR_COLORIZE_TYPE = 'Выбран метод оценивания по правилам лицея (0.65)'
+    console.log(OUT_STR_COLORIZE_TYPE);
+    return false
+}
+function StartWatch() { //начинает следить за изменением элемента с названием журнала
+    MUTATION_OBSERVER.observe(WATCH_ELEMENT, {
+        attributes: true,
+        characterData: true,
+        childList: true,
+        subtree: true,
+        attributeOldValue: true,
+        characterDataOldValue: true
+    });
+}
+
+function getHim() { // поиск таблицы журнала
     let tablePoint = document.querySelectorAll('table'); // получаем таблицу журнала
     if (tablePoint.length == 0) {
         setTimeout(getHim, 1000);
         console.log("Таблица ещё не загружена, ждём 1с");
     }
     else {
-        console.log("Таблица найдена");
-        console.log(tablePoint[0]);
-        colorizeTable(tablePoint[0].lastChild)
-        let watchPoint = document.querySelectorAll('main')[0].firstChild; // получаем таблицу журнала
-        mutationObserver.observe(watchPoint, {
-            attributes: true,
-            characterData: true,
-            childList: true,
-            subtree: false,
-            attributeOldValue: true,
-            characterDataOldValue: true
-        });
+        console.log('Таблица найдена');
+        let f = isMathRoundType(WATCH_ELEMENT.textContent)
+        colorizeTable(tablePoint[0].lastChild, f)
+        StartWatch()
     }
 }
 
-function insertButton() {
-    let tablePoint = document.querySelectorAll('main');
-    if (tablePoint.length == 0) {
+function insertButton() { // вставка кнопок управления
+    let mainSection = document.querySelectorAll('main');
+    if (mainSection.length == 0) {
         setTimeout(insertButton, 1000);
-        console.log("Div для кнопки не ныйден ждём 1с");
+        console.log("Div для кнопки не найден, ждём 1с");
     }
     else {
-        console.log("Ставим кнопку");
-        let targetDiv = tablePoint[0].firstChild.firstChild.firstChild.firstChild.firstChild
-        console.log(targetDiv);
-        let bu = document.createElement('button');
-        bu.style.cssText = 'padding: 6px 10px 10px 10px; background-color: #4c6ef5; border-radius: 8px; color: white; margin-left: 10px;';
-        bu.textContent = "Покрасить"
+        console.log("Ставим кнопки");
+        WATCH_ELEMENT = document.querySelectorAll('div>div>div>h6')[1].parentNode; // получаем див подписи текущего журнала
+        // console.log(watchPoint);
+        const styleEl = document.createElement('style');
+        styleEl.innerHTML = `
+        .licey24_but{
+        line-height: 0;
+        padding: 8px;
+        margin-left: -1px;
+        border-color: #d6d6df;
+        border-style: solid;
+        border-width: 1px;
+        color: #686A71;
+        }
+       .licey24_but:hover {
+        background: lightgrey;}`;
+        document.head.appendChild(styleEl);
+        let di = document.createElement('div');
+        di.style.cssText = 'box-shadow: 1px 1px 4px 0px rgba(0, 0, 0, 0.07); border-radius: 8px;';
+        let bu = document.createElement('button'); // кнопка принудительной покраски
+        bu.style.cssText = 'border-radius: 8px 0 0 8px;';
+        bu.classList.add('licey24_but');
+        bu.innerHTML = ' <svg class="bi-brush" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"> <path d="M15.825.12a.5.5 0 0 1 .132.584c-1.53 3.43-4.743 8.17-7.095 10.64a6.1 6.1 0 0 1-2.373 1.534c-.018.227-.06.538-.16.868-.201.659-.667 1.479-1.708 1.74a8.1 8.1 0 0 1-3.078.132 4 4 0 0 1-.562-.135 1.4 1.4 0 0 1-.466-.247.7.7 0 0 1-.204-.288.62.62 0 0 1 .004-.443c.095-.245.316-.38.461-.452.394-.197.625-.453.867-.826.095-.144.184-.297.287-.472l.117-.198c.151-.255.326-.54.546-.848.528-.739 1.201-.925 1.746-.896q.19.012.348.048c.062-.172.142-.38.238-.608.261-.619.658-1.419 1.187-2.069 2.176-2.67 6.18-6.206 9.117-8.104a.5.5 0 0 1 .596.04M4.705 11.912a1.2 1.2 0 0 0-.419-.1c-.246-.013-.573.05-.879.479-.197.275-.355.532-.5.777l-.105.177c-.106.181-.213.362-.32.528a3.4 3.4 0 0 1-.76.861c.69.112 1.736.111 2.657-.12.559-.139.843-.569.993-1.06a3 3 0 0 0 .126-.75zm1.44.026c.12-.04.277-.1.458-.183a5.1 5.1 0 0 0 1.535-1.1c1.9-1.996 4.412-5.57 6.052-8.631-2.59 1.927-5.566 4.66-7.302 6.792-.442.543-.795 1.243-1.042 1.826-.121.288-.214.54-.275.72v.001l.575.575zm-4.973 3.04.007-.005zm3.582-3.043.002.001h-.002z"> </path> <defs> <linearGradient id="MyGradient"> <stop offset="0%" stop-color="green" /> <stop offset="50%" stop-color="blue" /> <stop offset="100%" stop-color="red" /> </linearGradient> </defs> <style type="text/css"> .bi-brush{fill:url(#MyGradient)} </style> </svg> '
         bu.addEventListener("click", getHim);
-        targetDiv.append(bu);
+        di.append(bu);
+        let bu2 = document.createElement('button'); // кнопка вывода информации о обработке
+        //bu2.style.cssText = 'border-radius: 0 8px 8px 0;';
+        bu2.classList.add('licey24_but');
+        bu2.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/><path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/></svg>'
+        bu2.addEventListener("click", () => alert(`${OUT_STR_COLORIZE_TYPE}\n${OUT_STR_TRIMESTR_INFO}`));
+        di.append(bu2);
+        let bu3 = document.createElement('button'); // кнопка копирования в эксель
+        bu3.style.cssText = 'border-radius: 0 8px 8px 0;';
+        bu3.classList.add('licey24_but');
+        bu3.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-spreadsheet" viewBox="0 0 16 16"><path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V9H3V2a1 1 0 0 1 1-1h5.5zM3 12v-2h2v2zm0 1h2v2H4a1 1 0 0 1-1-1zm3 2v-2h3v2zm4 0v-2h3v1a1 1 0 0 1-1 1zm3-3h-3v-2h3zm-7 0v-2h3v2z"/></svg>'
+        bu3.addEventListener("click",downloadAsFile );
+        di.append(bu3);
+        WATCH_ELEMENT.append(di);
+        getHim();
     }
 }
-function colorizeTable(tableBody) {
-    console.log(tableBody);
-    let coutLessons = tableBody.childNodes[2].childNodes.length - 3 // колличество уроков
+function colorizeTable(tableBody, isMathRound = false) {
+    //console.log(tableBody);
+    let coutLessons = tableBody.childNodes[2].childNodes.length - 3 // количество уроков
     let needPoint = coutLessons > 15 ? 5 : 3; // сколько будет нужно оценок
-    
-    console.log("Уроков в триместре:" + coutLessons)
-    console.log("Оценок за триметр должнобыть не менее:" + needPoint)
+    OUT_STR_TRIMESTR_INFO = `Уроков в триместре: ${coutLessons}\nОценок за триметр должнобыть не менее: ${needPoint}`
+    console.log(OUT_STR_TRIMESTR_INFO);
+
     for (let i = 0; i < tableBody.childNodes.length; i++) {
         let rouNode = tableBody.childNodes[i] //строки таблицы
         if (rouNode.childNodes.length > 2) { // пропускаем пустые строки таблицы, возможно можно только первую
             let pointCount = 0; // количество оценок в строке считать
             let colorVal = ['#FF9999', '#FFFFCC', "#99CCFF", '#CCFFCC'] // варианты цветов
 
-            for (let j = 1; j < rouNode.childNodes.length-1; j++) {// пробегаем по ячейкам с оценками
+            for (let j = 1; j < rouNode.childNodes.length - 1; j++) {// пробегаем по ячейкам с оценками
                 let cellNode = rouNode.childNodes[j].firstChild
-                if (cellNode.hasChildNodes()) {// если есть оценка в ячейке (или н-ка)                  
+                if (cellNode.hasChildNodes()) {// если есть оценка в ячейке (или н-ка)
                     let point = cellNode.firstChild.textContent
                     if (['2', '3', '4', '5'].includes(point)) { // проверяем оценка это или "Н"
                         cellNode.style.backgroundColor = colorVal[point - 2] // ставим диву в ячейке цвет из массива по значению оценки -2 чтобы в индекс превратить
@@ -80,24 +137,60 @@ function colorizeTable(tableBody) {
             }
             let countPIndicSpan = rouNode.childNodes[0].firstChild.querySelector('div>div>span') // получаем номер ученика
             if (pointCount >= needPoint) { countPIndicSpan.style.backgroundColor = '#CCFFCC'; } // красим в зависимости от колличетва оценок
-                    else { countPIndicSpan.style.backgroundColor = '#FF9999'; }
-     
-            let itogPointItem = rouNode.childNodes[rouNode.childNodes.length-1].firstChild // получаем средний балл
+            else { countPIndicSpan.style.backgroundColor = '#FF9999'; }
+
+            let itogPointItem = rouNode.childNodes[rouNode.childNodes.length - 1].firstChild // получаем средний балл
             let point = parseFloat(itogPointItem.firstChild.textContent.replace(",", '.'))// переводим в число
-            //красим по правилам округления оценок
-            if (point<2.6) {
-                itogPointItem.style.backgroundColor = colorVal[0];            
+
+            if (isMathRound) {
+                //красим по правилам округления математики
+                if (point >= 4.5) {
+                    itogPointItem.style.backgroundColor = colorVal[3];
+                    continue
+                }
+                if (point >= 3.5) {
+                    itogPointItem.style.backgroundColor = colorVal[2];
+                    continue
+                }
+                if (point >= 2.5) {
+                    itogPointItem.style.backgroundColor = colorVal[1];
+                    continue
+                }
+                if (point < 2.5) {
+                    itogPointItem.style.backgroundColor = colorVal[0];
+                    continue
+                }
+
+            } else {
+                //красим по правилам округления оценок лицея
+                if (point >= 4.65) {
+                    itogPointItem.style.backgroundColor = colorVal[3];
+                    continue
+                }
+                if (point >= 3.6) {
+                    itogPointItem.style.backgroundColor = colorVal[2];
+                    continue
+                }
+                if (point >= 2.6) {
+                    itogPointItem.style.backgroundColor = colorVal[1];
+                    continue
+                }
+                if (point < 2.6) {
+                    itogPointItem.style.backgroundColor = colorVal[0];
+                    continue
+                }
             }
-            if (point>=2.6) {
-                itogPointItem.style.backgroundColor = colorVal[1];            
-            }
-            if (point>=3.6) {
-                itogPointItem.style.backgroundColor = colorVal[2];            
-            }
-            if (point>=4.65) {
-                itogPointItem.style.backgroundColor = colorVal[3];            
-            }
+
         }
     }
 }
-window.onload = function () { getHim(); insertButton(); };
+function downloadAsFile() {
+    let tablePoint = document.querySelectorAll('table')[0]; // получаем таблицу журнала
+    let a = document.createElement("a");
+    let file = new Blob([tablePoint.outerHTML], { type: 'text/html' });
+    a.href = URL.createObjectURL(file);
+    a.download = `ex.html`;
+    a.click();
+    a.remove()
+}
+window.onload = function () { insertButton(); };
--- testGetAllJornal.js
+++ testGetAllJornal.js
@@ -1,21 +1,22 @@
 // ==UserScript==
-// @name         New Userscript2
+// @name         Get Jornal
 // @namespace    http://tampermonkey.net/
-// @version      2024-11-20
+// @version      2025-02-02
 // @description  try to take over the world!
 // @author       You
-// @match        https://authedu.mosreg.ru/teacher/study-process/journal/grade/*
+// @match        https://authedu.mosreg.ru/teacher/study-process/journal/grade
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
-let i_predmet = 4
-let i_class = 10
+let i_predmet = 0
+let i_class = 0
 let urlsList = ""
 let copyTable = ""
 let megaArr = []
 let i_memSave = 0
 let flagTimeChangeClassNumber = false
+
 function expandPanel() {// получуаем выпадающий список журналов
     let findElem = ".JBeaw3_TEokaXcUhqpek"
     let targets = document.querySelectorAll(findElem)
@@ -62,11 +63,15 @@ function getURL() {
     else {
         i_predmet += 1;
         console.log(i_predmet)
-        let strWrite = getHeadTable(tablePoint[0].firstChild)
-        strWrite.unshift(window.location.href)
-        console.log(strWrite)
-        megaArr.push(strWrite)
-        console.log(megaArr)
+        // let strWrite = getHeadTable(tablePoint[0].firstChild)
+        // strWrite.unshift(window.location.href)
+        // console.log(strWrite)
+        // megaArr.push(strWrite)
+        // console.log(megaArr)
+        console.log(tablePoint)
+        console.log(tablePoint[0])
+        console.log(tablePoint[0].outerHTML)
+        downloadAsFile(tablePoint[0].outerHTML, `${i_class}-${i_predmet}`)
         history.back();
         i_memSave++;
         if (i_memSave==150) {
@@ -76,73 +81,12 @@ function getURL() {
         setTimeout(expandPanel, 2000);
     }
 }
-function getHeadTable(tableHead) {
-
-    let rouArr1 = []
-    // первая строка состоит из блока с названием предмета и именем учителя, затем названия месяцев, последний элемент это средний балл
-    let firstRou = tableHead.childNodes[0] //первая строка
-    let targetSpanList = firstRou.querySelectorAll('th>div>div>div>div>div>span')// получаем лист span с именем учителяи и названием предмета
-    let predmetName = targetSpanList[0].textContent // название предмета
-    // let ychitelName = targetSpanList[1].textContent // имя учителя
-    rouArr1.push(predmetName)
-    // let strOut = predmetName + "\t\t" // строка для хранения таблицы. Добавляем имя предмета и пропуск одной ячейки
-    for (let i = 1; i < firstRou.childNodes.length - 1; i++) { // проходим названия месяцев, все узлы строки кроме первого и последнего
-        let monfBloc = firstRou.childNodes[i] // блок с названием месяца
-        let countRepeat = parseInt(monfBloc.colSpan) // сколько ячеек нужно отступить после названия месяца
-        let monfName =  monfBloc.querySelector('div>span').textContent // получаем из блока название месяца и добавлем в строку таблицы
-        while (countRepeat != 0) { //заполяем название месяцев
-            rouArr1.push(monfName)
-            countRepeat--;
-          }
-        // strOut += "\t".repeat(countRepeat)// добавляем в строку нужное колличество пропусков ячеек
-    }
-
-    let rouArr2 = ["name"]
-    let triRou = tableHead.childNodes[2] // третья строка
-    for (let i = 0; i < triRou.childNodes.length; i++) {
-        let dateBloc = triRou.childNodes[i]
-        let countRouSpan = parseInt(dateBloc.rowSpan)
-        if (countRouSpan>1) {
-            rouArr2.push("-")
-        } else {
-            let date = dateBloc.querySelector('div>span').textContent
-            if (date=="") {
-                date = dateBloc.querySelectorAll('div>span')[1].textContent
-            }
-            rouArr2.push(date)
-        }
-   }
-
-    let newArr = [i_class, i_predmet, rouArr1[0]]
-    for (let index = 1; index < rouArr2.length; index++) {
-        let v
-        switch (rouArr1[index]) {
-            case "Ноябрь":
-              v=  "11"
-                break;
-            case "Декабрь":
-              v=  "12"
-                break;
-            case "Январь":
-              v=  "1"
-                break;
-            case "Февраль":
-              v=  "2"
-                break;
-
-            default:
-                v="0"
-                break;
-        }
-        if (rouArr2[index] !="-") {
-            newArr.push( rouArr2[index].toString().padStart(2, '0') + "."+ v.padStart(2, '0')+".2024")
-        }
-
-
-    }
-    console.log([rouArr1,rouArr2]);
-    // console.log(newArr);
-    return newArr
+function downloadAsFile(data, name = "example") {
+    let a = document.createElement("a");
+    let file = new Blob([data], { type: 'text/html' });
+    a.href = URL.createObjectURL(file);
+    a.download = `${name}.html`;
+    a.click();
+    a.remove()
 }
-
-setTimeout(expandPanel, 1000);
+setTimeout(expandPanel, 2000);
