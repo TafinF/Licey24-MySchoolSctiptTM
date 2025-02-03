08ab2d978a68f9d5cab0a0955df239df0144ffbb
Tafin <taf.f11@ya.ru>
2025-02-03 16:48:33 +0300

    добавлено ожидание при скачивании

--- testGetAllJornal.js
+++ testGetAllJornal.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         Get Jornal
 // @namespace    http://tampermonkey.net/
-// @version      2025-02-02
-// @description  try to take over the world!
+// @version      2025-02-03
+// @description  Сохраняет таблицы журналов
 // @author       You
 // @match        https://authedu.mosreg.ru/teacher/study-process/journal/grade
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
-let i_predmet = 0
-let i_class = 0
+let i_predmet = 16
+let i_class = 7
 let urlsList = ""
 let copyTable = ""
 let megaArr = []
@@ -33,7 +33,7 @@ function expandPanel() {// получуаем выпадающий список 
         console.log(findElem + " найден");
         console.log(targets[i_class]);
         targets[i_class].firstChild.click()
-        setTimeout(setClass, 2000);
+        setTimeout(setClass, 500);
     }
 }
 
@@ -42,7 +42,7 @@ function setClass() {// кликаем по журналу
     let findElem = ".mo3i0By7o2f2M_Erhktw"
     let targets = document.querySelectorAll(findElem)
     if (targets.length == 0) {
-        setTimeout(setClass, 2000);
+        setTimeout(setClass, 1000);
         console.log(findElem + "ещё не загружен, ждём 1с");
     }
     else {
@@ -56,32 +56,27 @@ function setClass() {// кликаем по журналу
     }
 }
 function getURL() {
-    let tablePoint = document.querySelectorAll('table'); // получаем таблицу журнала
+    let tablePoint = document.querySelectorAll('table'); // находим таблицу журнала
     if (tablePoint.length == 0) {
         setTimeout(getURL, 2000);
     }
     else {
         i_predmet += 1;
         console.log(i_predmet)
-        // let strWrite = getHeadTable(tablePoint[0].firstChild)
-        // strWrite.unshift(window.location.href)
-        // console.log(strWrite)
-        // megaArr.push(strWrite)
-        // console.log(megaArr)
-        console.log(tablePoint)
-        console.log(tablePoint[0])
-        console.log(tablePoint[0].outerHTML)
-        downloadAsFile(tablePoint[0].outerHTML, `${i_class}-${i_predmet}`)
+        setTimeout(getTable, 1000);
+}
+function getTable(){// получаем таблицу журнала
+    let tablePoint = document.querySelectorAll('table');
+    downloadAsFile(tablePoint[0].outerHTML, `${('00'+i_class).slice(-2)}-${('00'+i_predmet).slice(-2)}`)
         history.back();
         i_memSave++;
-        if (i_memSave==150) {
-            alert("150")
+        if (i_memSave==100) {
+            alert("100")
             i_memSave = 0
         }
-        setTimeout(expandPanel, 2000);
-    }
-}
-function downloadAsFile(data, name = "example") {
+        setTimeout(expandPanel, 1000);
+    }}
+function downloadAsFile(data, name = "example") { // скачиваем таблицу журнала
     let a = document.createElement("a");
     let file = new Blob([data], { type: 'text/html' });
     a.href = URL.createObjectURL(file);
