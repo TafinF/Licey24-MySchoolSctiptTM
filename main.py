d7e8dc19268feaf0cea619c21a4643f99cc746ca
Tafin <taf.f11@ya.ru>
2025-02-04 15:34:50 +0300

    скачивание журналов с асинхронностью. добавлены кнопки управления загрузкой. добалено сохранение позиции скачивания при перезагрузке страницы. качаем ссылку на журнал, разрезаем номер класса и предмет. домик без дз распознаётся

--- main.py
+++ main.py
@@ -9,119 +9,152 @@ locale.setlocale(
     locale="Russian" 
 )
 
-
-
-#datetime.datetime.strptime('Ноябрь-2013',"%B-%Y")
-# синий -  MV57GCYYcPhOHkSQsK9F EoyGTa_NIo_M5boZkXU0
-# рыжий -   MV57GCYYcPhOHkSQsK9F ULjoCgrIj4yNfBKVwZYe
-# красный -    MV57GCYYcPhOHkSQsK9F d8JBiaXrvL8XVZ58xn9g
-# серый  -     MV57GCYYcPhOHkSQsK9F
-def get_color_by_string(input_str):
+def get_color_by_string(input_str): # Определяем цвет домика по классу
     if "EoyGTa_NIo_M5boZkXU0" in input_str:
-        return "с"
+        return "с" # синий
     elif "ULjoCgrIj4yNfBKVwZYe" in input_str:
-        return "р"
+        return "р" # рыжий
     elif "d8JBiaXrvL8XVZ58xn9g" in input_str:
-        return "к"
+        return "к" # красный
+    elif "без ДЗ" in input_str:
+        return "б" # синий без ДЗ
     else:
-        return "н"
+        return "н" # серый
 
-def get_home_list(path):
+def get_home_list(path): # Полуйчаем данные из файла с таблицей
     with open(path, "r", encoding='UTF-8') as f:       
         contents = f.read()   
         soup = BeautifulSoup(contents, 'lxml')
-        rowList = soup.find_all('tr',{"class": "f0eMcueVzidF1N9SAffu"})
-        list_mon = []
-        for i in rowList[0].find_all('th'): # Месяцы
-            z = i.find("span").text # Название месяца
+        url = soup.find('div',{"class": "URL"}) # ищем первый див с ссылкой на этот журнал
+        rowList = soup.find_all('tr',{"class": "f0eMcueVzidF1N9SAffu"}) # получаем все строки таблицы
+        list_mon = [] #  лист для месяцев
+        for i in rowList[0].find_all('th'): # Месяцы, ячейки первой строки таблицы
+            z = i.find("span").text # Название месяца с ячейки
             l = int(i.get('colspan', 1)) # Сколько столбцов займёт
-            for q in range(l): # Вставляем стокль столбцов с этим месяцем
+            for a in range(l): # Вставляем столько столбцов с этим месяцем
                 list_mon.append(z)
-        list_day = ['']
-        list_home_class = ['']
-        for i in rowList[2].find_all('th'): # даты с домиками
-            z = i.find("span").text # Число
+        list_day = ['']# лист для дней
+        list_home_class = ['']# лист для хранения классов домиков
+        for i in rowList[2].find_all('th'): # даты с домиками, ячейки третьей строки
+            z = i.find("span").text # Число (дата)
             q = i.find('svg')['class'] # классы у домика
+            t = i.find('svg').contents[0]['d'][:7] # получаем первые 7 символов пути кривой svg
+            if 'M20.354' in t: # если эти символы как у перечёркнутого домика
+                q = ['без ДЗ'] # указываем класс. что он без ДЗ. в оригинале уникального класса нет
             l = int(i.get('colspan', 1)) # Сколько столбцов займёт
-            for a in range(l): # Вставляем стокль столбцов с этим днём
+            # TODO: переделать копирование дней, оно скореее всего вносит ошибки и мешает
+            for a in range(l): # Вставляем столько столбцов с этим днём
                 list_day.append(z)
                 list_home_class.append(q)
+        # лист месяцев длинее чем дней, добавляем недостающее для уравнивания длины
+        # TODO: может проще обрезать лист месяцев
         list_day.append('')
         list_day.append('')
         list_home_class.append('')
         list_home_class.append('')
-        out_list = []
-        for i in range(len(list_mon)):
+        out_list = []# выходной список
+        for i in range(len(list_mon)):# перебираем список с месяцами
             try:
                 if (list_mon[i]=="Ноябрь" or list_mon[i]=="Декабрь"):
                     year = '2024'
                 else:
                     year = '2025' 
-                date = datetime.datetime.strptime(f'{list_day[i]}-{list_mon[i]}-{year}',"%d-%B-%Y")
-                color = get_color_by_string(''.join(list_home_class[i]))
-            except:
+                date = datetime.datetime.strptime(f'{list_day[i]}-{list_mon[i]}-{year}',"%d-%B-%Y")# получаем дату из списка дня и месяца
+                color = get_color_by_string(''.join(list_home_class[i]))# получаем цвет домика из класса
+            except:# если дата не получилась, значит это не уроки а итог, средний балл, назвние и т.д. пишем пустоту
                 date = ''
                 color = ''
 
-            out_list.append([date,color])
-        name_jornal = ' '.join(list_mon[0].split())
-        out_list[0] = name_jornal
+            out_list.append([date,color])# добавляем запись о уроке в выходной лист
+        #name_jornal = list_mon[0]# получаем имя журнала из первой строки первой ячейки (там и месяцы)
+        out_list[0] = list_mon[0]# записываем имя журнала из первой строки первой ячейки (там и месяцы) в выходной лист
+        out_list.insert(0, url.text)# добавляем в начало url этого журнала
+        # отрезаем лишние элементы если они есть (есть пусные журналы, там пустых не будет)
         if(len(out_list)>1):
             out_list.pop()
         if(len(out_list)>1):
             out_list.pop()
         return out_list
 
-def date_range(start_date, end_date):
+def date_range(start_date, end_date): # генерируем лист с датам в диапазоне от-до
     for n in range(int((end_date - start_date).days)):
         yield start_date + datetime.timedelta(n)
 
+def split_class(text = 'Иностранный (немецкий) язык 1-М'): # разделяем название предмета и класс
+    sp_t = text.split('-')# для большинства журналов литера от буквы отделена -
+    if len(sp_t)==2:# если получилось 2 куска текста, значит это этот слуяай
+        sp_t[1] = sp_t[0][-1] + sp_t[1]# переносим цифру класса к букве
+        sp_t[0] = sp_t[0][:-1]# удаляем цифру
+        if sp_t[0][-1] != ' ': # если далее опять идёт цифра значит это 10 или 11 класс, перемещаем ещё цифру
+            sp_t[1] = sp_t[0][-1] + sp_t[1]
+            sp_t[0] = sp_t[0][:-1]
+        return sp_t
+    # отдельные случаи
+    if '7 НДО Дубовая' in text:
+        sp_t = text.split('7 НДО Дубовая')
+        return  [sp_t[0],'7 НДО Дубовая']
+    if 'Немецкий язык' in text:
+        return  [text[:len(text)-3],text[:-3]]
+    if 'Физическая культура' in text:
+        return  [text[:len(text)-2],text[:-2]]
+    if 'Геометрия 10А общие уроки' in text:
+        return  ['Геометрия ','10А общие уроки']
+    if 'Русская словесность 10А общий урок' in text:
+        return  ['Русская словесность ','10А общий урок']
+    pass
+
 # Устанавливаем начальную и конечную даты
-start_date = datetime.datetime(2024, 1, 23)
+start_date = datetime.datetime(2024, 11, 23)
 end_date = datetime.datetime(2025, 2, 16)
 
-# Создаем генератор для получения списка дат
+# создаём список из всех дат и добавляем в его начало заголовки
 dates = list(date_range(start_date, end_date))
+dates.insert(0, 'Ссылка')
+dates.insert(0, 'Класс')
 dates.insert(0, 'Название журнала')
-dates.insert(0, 'Имя файла сохранения')
+dates.insert(0, 'Имя файла')
+
 # Указываем путь к папке, где нужно искать файлы
-folder_path = r'C:\Users\Учитель\Documents\0Code\Licey24-JornalParser\Licey24-JornalParser\jornal'
-folder_path = r'C:\Users\Учитель\Documents\0Code\Licey24-JornalParser\Licey24-JornalParser\1'
+folder_path = r'C:\Users\Учитель\Documents\0Code\Licey24-MySchoolSctiptTM\jornal'
+folder_path = r'C:\Users\Учитель\Documents\0Code\Licey24-MySchoolSctiptTM\1'
 
 # Получаем список всех файлов с расширением .html в данной папке
 html_files = glob(os.path.join(folder_path, '*.html'))
 
-out_table = []
+out_table = []# общая таблица всех классов
 out_table.append(dates)
 # Перебираем найденные файлы и обрабатываем
 for file in html_files:
-    relative_path = os.path.relpath(file, folder_path)
-    data_jornal = get_home_list(file)
-    newRow  = []
-    for day in dates:
-        f = True
-        for i in data_jornal:
-            if day==i[0]:
-                newRow.append(i[1])
-                f= False
+    relative_path = os.path.relpath(file, folder_path)# относительный пути он же имя файла
+    data_jornal = get_home_list(file)# данные из этого файла
+    newRow  = []# будущая строка таблицы
+    for day in dates:# перебираем все даты
+        f = True # флаг наличия урока в эту дату
+        for i in data_jornal: # перебираем все уроки в журнале
+            if day==i[0]:# если дата строки таблицы и дата в журнале совпала, тоесть в этот день есть урок этого журнала
+                newRow.append(i[1])# добавляем обозначение цвета домика
+                f= False # ставим флаг, что нашли такую дату
                 break
-        if f:
+        if f: # если не нашли такой даты в журнале, ставим точку
            newRow.append('.')
-    cut_jornal_name =  data_jornal[0].split('УП')[0]
-    newRow[1] = cut_jornal_name
-    newRow[0] = relative_path
-    out_table.append(newRow)
+    cut_jornal_name =  data_jornal[1].split('УП')[0]# отрезаем лишнее от названия журнала
+    split_jornal_name = split_class(cut_jornal_name)# разделяем название журнала на класс и предмет
+    newRow[3] = data_jornal[0] # добавляем url
+    newRow[2] = split_jornal_name[1] # добаляем номер класса
+    newRow[1] = split_jornal_name[0] # добаляем название предмета
+    newRow[0] = relative_path # добавляем название файла сохранения
+    out_table.append(newRow) # добаляем строку в таблицу
             
 
 def save_array_to_file(array, filename):
-    # Соединяем элементы массива в строку через табуляцию
     out_str  = ''
     for i in array:
         array_str = '\t'.join(map(str, i))  # Преобразуем каждый элемент в строку и объединяем их через \t
-        out_str = out_str + array_str + "\n"
+        out_str = out_str + array_str + "\n" # добаляем строку в итоговую строку через перенос строки
     
-    # Сохраняем строку в файл
+    # Сохраняем выходную строку в файл
     with open(filename, 'w', encoding='UTF-8') as file:
         file.write(out_str)
 
-save_array_to_file(out_table,'out.txt')
+
+save_array_to_file(out_table,'out.txt')  # записываем выходную строку в файл
--- testGetAllJornal.js
+++ testGetAllJornal.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Get Jornal
 // @namespace    http://tampermonkey.net/
-// @version      2025-02-03
+// @version      2025-02-04
 // @description  Сохраняет таблицы журналов
 // @author       You
 // @match        https://authedu.mosreg.ru/teacher/study-process/journal/grade
@@ -9,73 +9,72 @@
 // @grant        none
 // ==/UserScript==
 
-let i_predmet = 16
-let i_class = 7
-let urlsList = ""
-let copyTable = ""
-let megaArr = []
-let i_memSave = 0
-let flagTimeChangeClassNumber = false
+let I_PREDMET = 0
+let I_CLASS = 0
 
-function expandPanel() {// получуаем выпадающий список журналов
-    let findElem = ".JBeaw3_TEokaXcUhqpek"
-    let targets = document.querySelectorAll(findElem)
-    if (targets.length == 0) {
-        setTimeout(expandPanel, 2000);
-        console.log(findElem + "ещё не загружен, ждём 1с");
-    }
-    else {
-        if (flagTimeChangeClassNumber) {
-            i_class = i_class + 1
-            flagTimeChangeClassNumber = false
-            i_predmet = 0
+if (localStorage.getItem('l24_stor') != null) {
+    let sxron = localStorage.getItem('l24_stor').split('-')
+    I_PREDMET = parseInt(sxron[0])
+    I_CLASS = parseInt(sxron[1])
+}
+let I_RAM_SAVE = 0 //Ограничитель количества скачиваемых журналов
+let FLAG_TimeChangeClassNumber = false
+
+let FLAG_stop_download = true
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms)); // для преостановки программы на время
+
+async function q() {
+    while (I_RAM_SAVE <= 100) { // скачиваем пока не будет 100 шт
+        if (FLAG_stop_download) {
+            return
         }
-        console.log(findElem + " найден");
-        console.log(targets[i_class]);
-        targets[i_class].firstChild.click()
-        setTimeout(setClass, 500);
+        let jornal_rows = await find_Elemremt(".JBeaw3_TEokaXcUhqpek") // получаем строки с выпадающим меню классов
+        //console.log(jornal_rows);
+        jornal_rows[I_CLASS].firstChild.click() // кликаем на нужную строку
+        if (FLAG_TimeChangeClassNumber) { // если все классы перебраны
+            I_CLASS = I_CLASS + 1 // переходим к следующему классу
+            FLAG_TimeChangeClassNumber = false // возвращаем флаг смены класса на место
+            I_PREDMET = 0 // выставляем номер выбираемого журнала в 0
+        }
+        await sleep(500)
+        let predmet_list = await find_Elemremt(".mo3i0By7o2f2M_Erhktw") // получаем список всех журналов в данном классе
+        //console.log(predmet_list);
+        if (predmet_list.length - 1 == I_PREDMET) { // если это последний журнал в списке то ставим флаг для смены класса
+            FLAG_TimeChangeClassNumber = true
+        }
+        predmet_list[I_PREDMET].click() // кликаем на нужный журнал
+        I_PREDMET += 1;
+        await find_Elemremt("table") // дожидаемся таблицу журнала
+        await sleep(2000) // ждём для прогрузки цветов домиков
+        let red_home = await find_Elemremt('.d8JBiaXrvL8XVZ58xn9g')
+        if (red_home.length>5) {
+            await sleep(3000)
+        }
+        let red_home2 = await find_Elemremt('.d8JBiaXrvL8XVZ58xn9g')
+        if (red_home2.length>5) {
+            await sleep(15000)
+        }
+        let tablePoint = document.querySelectorAll('table'); // получаем таблицу журнала
+        let data = `<div class="URL">${window.location.href}</div>${tablePoint[0].outerHTML}`
+        downloadAsFile(data, `${('00' + I_CLASS).slice(-2)}-${('00' + I_PREDMET).slice(-2)}`) // скачиваем
+        history.back();// возвращаемся на страницу выбора журнала
+        I_RAM_SAVE++;
+        console.log(`Загружено журналов: ${I_RAM_SAVE}`);
+        localStorage.setItem('l24_stor', `${I_PREDMET}-${I_CLASS}`);
     }
 }
 
+async function find_Elemremt(target_elem_selector) { // находим нужный элемент
+    let targets = document.querySelectorAll(target_elem_selector) // получаем элемент по селектору
+    while (targets.length == 0) { // продолжнаем пока ненайдём такие элементы
+        await sleep(500) // ждём
+        targets = document.querySelectorAll(target_elem_selector) // получаем элемент по селектору
+        //console.log(targets);
+    }
+    return targets
+}
 
-function setClass() {// кликаем по журналу
-    let findElem = ".mo3i0By7o2f2M_Erhktw"
-    let targets = document.querySelectorAll(findElem)
-    if (targets.length == 0) {
-        setTimeout(setClass, 1000);
-        console.log(findElem + "ещё не загружен, ждём 1с");
-    }
-    else {
-        if (targets.length-1 == i_predmet) {
-            flagTimeChangeClassNumber = true
-        }
-        console.log(findElem + "найден");
-        console.log(targets[i_predmet]);
-        targets[i_predmet].click()
-        setTimeout(getURL, 2000);
-    }
-}
-function getURL() {
-    let tablePoint = document.querySelectorAll('table'); // находим таблицу журнала
-    if (tablePoint.length == 0) {
-        setTimeout(getURL, 2000);
-    }
-    else {
-        i_predmet += 1;
-        console.log(i_predmet)
-        setTimeout(getTable, 1000);
-}
-function getTable(){// получаем таблицу журнала
-    let tablePoint = document.querySelectorAll('table');
-    downloadAsFile(tablePoint[0].outerHTML, `${('00'+i_class).slice(-2)}-${('00'+i_predmet).slice(-2)}`)
-        history.back();
-        i_memSave++;
-        if (i_memSave==100) {
-            alert("100")
-            i_memSave = 0
-        }
-        setTimeout(expandPanel, 1000);
-    }}
 function downloadAsFile(data, name = "example") { // скачиваем таблицу журнала
     let a = document.createElement("a");
     let file = new Blob([data], { type: 'text/html' });
@@ -84,4 +83,54 @@ function downloadAsFile(data, name = "example") { // скачиваем табл
     a.click();
     a.remove()
 }
-setTimeout(expandPanel, 2000);
+
+async function setButton() {
+    let nav_elem = await find_Elemremt('div>div>div>nav'); // получаем див навигации
+    //console.log(nav_elem);
+    const styleEl = document.createElement('style');
+    styleEl.innerHTML = `
+     .licey24_but{
+    line-height: 0;
+    padding: 8px;
+    margin-left: -1px;
+    border-color: #d6d6df;
+    border-style: solid;
+    border-width: 1px;
+    color: #d5d6da;
+    border-radius: 5px;
+    }
+   .licey24_but:hover {
+    background: lightgrey;
+    color: black;}`;
+    document.head.appendChild(styleEl);
+    let di = document.createElement('div');
+    let bu = document.createElement('button'); // кнопка
+    bu.innerText = "скачивание"
+    bu.classList.add('licey24_but');
+    bu.addEventListener("click", start_stop_download);
+    di.append(bu);
+    let bu2 = document.createElement('button'); // кнопка
+    bu2.innerText = "задать номер"
+    bu2.classList.add('licey24_but');
+    bu2.addEventListener("click", defalt_val);
+    di.append(bu2);
+
+    nav_elem[0].append(di);
+}
+
+function start_stop_download() {
+    setTimeout(q, 1000)
+    if (FLAG_stop_download) {
+        FLAG_stop_download = false
+    } else {
+        FLAG_stop_download = true
+    }
+}
+function defalt_val() {
+    let result = prompt("задание значений:  предмет-класс", "0-0");
+    let sxron = result.split('-')
+    I_PREDMET = parseInt(sxron[0])
+    I_CLASS = parseInt(sxron[1])
+    localStorage.setItem('l24_stor', `${I_PREDMET}-${I_CLASS}`);
+}
+setTimeout(setButton, 2000);
